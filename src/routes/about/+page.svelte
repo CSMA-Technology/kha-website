501d824aae5e601920054c4e11509acdfd8d2508
Stephanie Cruz <exscruzme@gmail.com>
2022-11-02 16:51:53 -0400

    added instafeed

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,71 +1,98 @@
+<head>
+  <title>About - KHA</title>
+</head>
+
 <script lang="ts">
   import placeholder from '$lib/assets/avatars/avatar-placeholder.png';
-    import TeamMember from '$lib/components/TeamMember.svelte';
+  import TeamMember from '$lib/components/TeamMember.svelte';
+  import Instafeed  from 'instafeed.js';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    var feed = new Instafeed({
+      accessToken: 'IGQVJWRmZAPYkhXNVFLTXRya090TXdCeXFIOGJpYVc5UVYxLWZA1TUFOaEJRQWltZAlp2RGlGRzc2T1Eya3A5TkNDNzdUd3pJOEZAoRnU5VDBBUTg4YkxtdzZA5ZADl3REQ4VmdUTmNicXhtc3hWZAWtSZAkF3awZDZD',
+      limit: 3,
+      template: '<div class="post"><a href="{{link}}"><img title="{{caption}}" src="{{image}}" width="200" /></a></div>'
+    });
+    feed.run();
+  });
 </script>
 
-<head>
-  <title>About - KHA</title>
-</head>
-
-<head>
-  <title>About - KHA</title>
-</head>
-
 <h1 class="page-heading">About</h1>
-<section class="container">
-  <h2 class="page-subheading">Our Mission</h2>
-  <p>
-    To serve and inform the community of the Village of Kendale, Kendale Country Club Estates and Kendale South, to promote safety, foster social and  community activities, 
-    to enhance and protect our environment and to further interaction of the Association Membership  while preserving the quality of life and sense of community in our neighborhood.
-  </p>
-
-  <h2 class="page-subheading">Our Vision</h2>
-  <p>
-    We envision a community that offers an inspiring lifestyle and quality of life, and is a desirable place in which to live and enjoy the benefits of family, friends and community.
-  </p>
-</section>
 
 <section class="container">
-  <h2 class="page-subheading">Our Team</h2>
-  <div class="team">
-    <TeamMember
-      img={placeholder}
-      position="President"
-      name="Ernie Bascuas"
-    />
-    <TeamMember
-      img={placeholder}
-      position="Vice President"
-      name="Zuriel Carbo"
-    />
-    <TeamMember
-      img={placeholder}
-      position="Treasurer"
-      name="Migdy Moya"
-    />
-    <TeamMember
-      img={placeholder}
-      position="Corresponding Secretary"
-      name="Patty Gonzalez"
-    />
-    <TeamMember
-      img={placeholder}
-      position="Past President"
-      name="Nancy Lyons"
-    />
-  </div>
-  <h2 class="page-subheading">We need your help!</h2>
-  <p>There are many positions open! If you wish to volunteer, send us a message from the <a href="/contact">contact</a> page.</p>
+  <section class="content">
+    <h2 class="page-subheading">Our Mission</h2>
+    <p>
+      To serve and inform the community of the Village of Kendale, Kendale Country Club Estates and Kendale South, to promote safety, foster social and  community activities, 
+      to enhance and protect our environment and to further interaction of the Association Membership  while preserving the quality of life and sense of community in our neighborhood.
+    </p>
+  
+    <h2 class="page-subheading">Our Vision</h2>
+    <p>
+      We envision a community that offers an inspiring lifestyle and quality of life, and is a desirable place in which to live and enjoy the benefits of family, friends and community.
+    </p>
+    <h2 class="page-subheading">Our Team</h2>
+    <div class="team">
+      <TeamMember
+        img={placeholder}
+        position="President"
+        name="Ernie Bascuas"
+      />
+      <TeamMember
+        img={placeholder}
+        position="Vice President"
+        name="Zuriel Carbo"
+      />
+      <TeamMember
+        img={placeholder}
+        position="Treasurer"
+        name="Migdy Moya"
+      />
+      <TeamMember
+        img={placeholder}
+        position="Corresponding Secretary"
+        name="Patty Gonzalez"
+      />
+      <TeamMember
+        img={placeholder}
+        position="Past President"
+        name="Nancy Lyons"
+      />
+    </div>
+    <h2 class="page-subheading">We need your help!</h2>
+    <p>There are many positions open! If you wish to volunteer, send us a message from the <a href="/contact">contact</a> page.</p>
+  </section>
+  <div id="instafeed" class="instagram"></div>
 </section>
 
 <style> 
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  
   .team {
     display: flex; 
     flex-flow: row wrap;
     justify-content: space-between;
   }
-  .break {
-    flex-basis: 100%;
-    height: 0;
+
+  .instagram {
+    margin: 20px;
+    display: flex;
+    flex-flow: row wrap;
+    font-size: 0.7rem;
+  }
+
+  .post {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .content {
+    margin: 0px 20px 20px 0px;
+    text-align: left;
   }
 </style>
