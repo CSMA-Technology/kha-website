6ef3117c6620ff84f1d867f6fddb037e80d6423a
Stephanie Cruz <exscruzme@gmail.com>
2022-11-09 14:44:41 -0500

    styling instagram section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,14 +7,14 @@
   import TeamMember from '$lib/components/TeamMember.svelte';
   import khaLogo from '$lib/assets/kha-logo.gif';
   import type { PageServerData } from './$types';
-  import { Carousel, CarouselControl, CarouselItem, CarouselCaption } from 'sveltestrap';
+  import { Carousel, CarouselControl, CarouselItem } from 'sveltestrap';
   
   export let data: PageServerData;
   let items = data.posts;
   let activeIndex = 0;
 </script>
 
-<h1 class="page-heading">About</h1>
+<h1 class="page-heading">About KHA</h1>
 <section class="container">
   <section class="content">
     <h2 class="page-subheading">Our Mission</h2>
@@ -55,39 +55,48 @@
         name="Nancy Lyons"
       />
     </div>
-    <h2 class="page-subheading">We need your help!</h2>
-    <p>There are many positions open! If you wish to volunteer, send us a message from the <a href="/contact">contact</a> page.</p>
   </section>
   <section class="instagram">
     <h2 class="page-subheading">Follow us on Instagram!</h2>
-    <div class="username">
-      <img alt="kha instagram profile" class="avatar" src={khaLogo} />
-      <p>{items[0].username}</p>
-    </div>
-    <Carousel {items} bind:activeIndex>
-      <div class="carousel-inner">
-        {#each items as item, index}
-          <CarouselItem bind:activeIndex itemIndex={index}>
-            <a href={item.permalink} target="_blank" rel="noreferrer">
-              {#if item.media_type=="VIDEO"}
-                <img src={item.thumbnail_url} alt="instagram post" class="d-block w-100" />
-              {:else}
-                <img src={item.media_url} alt="instagram post" class="d-block w-100" />
-              {/if}
-            </a>
-          </CarouselItem>
-        {/each}
+    <p>Stay up to date on all of the events around our beautiful community.</p>
+    <div class="post">
+      <div class="username">
+        <img alt="kha instagram profile" class="avatar" src={khaLogo} />
+        <p>{items[0].username}</p>
       </div>
-      <CarouselControl direction="prev" bind:activeIndex {items} />
-      <CarouselControl direction="next" bind:activeIndex {items} />
-    </Carousel>
-    <p class="caption">{items[activeIndex].caption}</p>
+      <Carousel {items} bind:activeIndex>
+        <div class="carousel-inner">
+          {#each items as item, index}
+            <CarouselItem bind:activeIndex itemIndex={index}>
+              <a href={item.permalink} target="_blank" rel="noreferrer">
+                {#if item.media_type=="VIDEO"}
+                  <img src={item.thumbnail_url} alt="instagram post" class="d-block w-100" />
+                {:else}
+                  <img src={item.media_url} alt="instagram post" class="d-block w-100" />
+                {/if}
+              </a>
+            </CarouselItem>
+          {/each}
+        </div>
+        <div class="carousel-control-left">
+          <CarouselControl direction="prev" bind:activeIndex {items} />
+        </div>
+        <div class="carousel-control-right">
+          <CarouselControl direction="next" bind:activeIndex {items}  />
+        </div>
+      </Carousel>
+      <p class="caption">{items[activeIndex].caption}</p>
+    </div>
   </section>
+</section>
+<section class="volunteer">
+  <h2 class="page-subheading">We need your help!</h2>
+  <p>There are many positions open! If you wish to volunteer, send us a message from the <a href="/contact">contact</a> page.</p>
 </section>
 
 <style> 
   .container {
-    max-width: 70%;
+    max-width: 60%;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -100,22 +109,37 @@
   }
 
   .content {
-    margin: 0px 20px 20px 0px;
+    margin: 20px;
     text-align: left;
   }
 
   .instagram {
-    margin: 0px 20px 20px 0px;
+    margin: 20px;
     height: 300px;
     max-width: 300px;
   }
 
+  .carousel-control-left {
+    filter: invert(100%);
+    position: relative;
+    bottom: 135px;
+    right: 8px;
+  }
+
+  .carousel-control-right {
+    filter: invert(100%);
+    position: relative;
+    bottom: 135px;
+    left: 8px;
+  }
+
   .carousel-inner {
-    height: 300px;
+    height: 270px;
     width: 300px;
-    background-color: black;
+    background-color: white;
     display: flex;
     align-items: center;
+    padding: 0px 30px;
   }
 
   .username {
@@ -126,6 +150,7 @@
     color: black; 
     padding: 5px;
     border-radius: 10px 10px 0px 0px;
+    padding: 10px 30px 0px;
   }
 
   .username p {
@@ -152,5 +177,10 @@
     white-space: nowrap;
     overflow: hidden;
     text-align: left;
+    padding: 0px 30px 15px;
+  }
+
+  .post {
+    margin: 20px 0px;
   }
 </style>
