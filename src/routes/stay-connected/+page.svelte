5a2a805f596cf7710d0d71c96a0d3d2b3c10436a
Adrian Moya <moyaa1022@gmail.com>
2023-06-08 16:27:17 +0000

    fix(subscription): adding existing users to the new list

--- src/routes/stay-connected/+page.svelte
+++ src/routes/stay-connected/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import FormContainer from "$lib/components/FormContainer.svelte";
   import { submitFormToNetlify } from "$lib/utils";
-  import InstagramPreview, {
-    type Post,
-  } from "$lib/components/InstagramPreview.svelte";
+  import InstagramPreview, { type Post } from "$lib/components/InstagramPreview.svelte";
   import { onMount } from "svelte";
   import { enhance } from "$app/forms";
   import type { ActionData } from "./$types";
@@ -58,16 +56,9 @@
   <section class="flex-item">
     <div class="card">
       <h2 class="page-subheading">Send Us a Message</h2>
-      <p>
-        We'd love to hear from you! Use the below form to send us a message, and
-        someone from the Board will get back to you shortly.
-      </p>
+      <p>We'd love to hear from you! Use the below form to send us a message, and someone from the Board will get back to you shortly.</p>
       <FormContainer formStatus={contactFormStatus}>
-        <form
-          on:submit={onContactFormSubmit}
-          class="contact-form"
-          data-netlify="true"
-          name="contact-form">
+        <form on:submit={onContactFormSubmit} class="contact-form" data-netlify="true" name="contact-form">
           <input type="hidden" name="form-name" value="contact-form" />
           <div class="form-row">
             <label class="form-label">
@@ -76,11 +67,7 @@
             </label>
             <label class="form-label">
               Email
-              <input
-                class="form-text-input"
-                type="email"
-                required
-                name="email" />
+              <input class="form-text-input" type="email" required name="email" />
             </label>
           </div>
           <div class="form-row">
@@ -101,39 +88,24 @@
               <textarea class="form-text-input" name="message" />
             </label>
           </div>
-          <button
-            disabled={!contactFormButtonEnabled}
-            class="primary-button form-submit-button">Submit</button>
+          <button disabled={!contactFormButtonEnabled} class="primary-button form-submit-button">Submit</button>
         </form>
       </FormContainer>
     </div>
     <div class="card" id="subscribeForm">
       <h2 class="page-subheading">Subscribe to Our Emails</h2>
-      <p>
-        Stay up to date on KHA events and community news by signing up for our
-        email newsletters!
-      </p>
+      <p>Stay up to date on KHA events and community news by signing up for our email newsletters!</p>
       <FormContainer
         formStatus={subscribeFormStatus}
         successMessage={"Thank you for subscribing!"}
-        errorMessage={form?.subscriptionErrorMessage ||
-          "There was an error subscribing, please try again later."}>
-        <form
-          method="POST"
-          action="?/subscribe"
-          class="contact-form"
-          on:submit={onSubscribeFormSubmit}
-          use:enhance>
+        errorMessage={"There was an error subscribing, please try again later."}>
+        <form method="POST" action="?/subscribe" class="contact-form" on:submit={onSubscribeFormSubmit} use:enhance>
           <div>
             <input type="hidden" name="nonce" />
             <div class="form-row">
               <label class="form-label">
                 Email
-                <input
-                  type="email"
-                  class="form-text-input"
-                  name="email"
-                  required />
+                <input type="email" class="form-text-input" name="email" required />
               </label>
               <label class="form-label">
                 Name (Optional)
@@ -141,11 +113,7 @@
               </label>
             </div>
             <p>
-              <input
-                class="primary-button form-submit-button"
-                disabled={!subscribeFormButtonEnabled}
-                type="submit"
-                value="Subscribe" />
+              <input class="primary-button form-submit-button" disabled={!subscribeFormButtonEnabled} type="submit" value="Subscribe" />
             </p>
           </div>
         </form>
@@ -153,18 +121,9 @@
     </div>
     <div class="card">
       <h2 class="page-subheading">Community Service</h2>
-      <p>
-        We offer various opportunities for high school students to earn service
-        hours! Leave your information below and we will be in touch.
-      </p>
-      <FormContainer
-        formStatus={serviceFormStatus}
-        successMessage="Thank you, we have received your submission! We will be in touch.">
-        <form
-          on:submit={onServiceFormSubmit}
-          class="contact-form"
-          data-netlify="true"
-          name="service-hours-form">
+      <p>We offer various opportunities for high school students to earn service hours! Leave your information below and we will be in touch.</p>
+      <FormContainer formStatus={serviceFormStatus} successMessage="Thank you, we have received your submission! We will be in touch.">
+        <form on:submit={onServiceFormSubmit} class="contact-form" data-netlify="true" name="service-hours-form">
           <input type="hidden" name="form-name" value="service-hours-form" />
           <div class="form-row">
             <label class="form-label">
@@ -173,25 +132,14 @@
             </label>
             <label class="form-label">
               Email
-              <input
-                class="form-text-input"
-                type="email"
-                required
-                name="email" />
+              <input class="form-text-input" type="email" required name="email" />
             </label>
             <label class="form-label">
               Phone
-              <input
-                class="form-text-input"
-                type="tel"
-                required
-                name="phone"
-                pattern={"[0-9-() ]{10,15}"} />
+              <input class="form-text-input" type="tel" required name="phone" pattern={"[0-9-() ]{10,15}"} />
             </label>
           </div>
-          <button
-            disabled={!serviceFormButtonEnabled}
-            class="primary-button form-submit-button">Submit</button>
+          <button disabled={!serviceFormButtonEnabled} class="primary-button form-submit-button">Submit</button>
         </form>
       </FormContainer>
     </div>
@@ -200,13 +148,11 @@
   <section class="flex-item instagram">
     <h2 class="page-subheading">Follow us on Instagram!</h2>
     <p class="instagram-subtitle">
-      Stay up to date on all of the events around our beautiful community by
-      following <a
+      Stay up to date on all of the events around our beautiful community by following <a
         style="color: black"
         rel="noreferrer"
         target="_blank"
-        href="https://www.instagram.com/kendale_hoa"
-        ><strong>@kendale_hoa</strong></a>
+        href="https://www.instagram.com/kendale_hoa"><strong>@kendale_hoa</strong></a>
     </p>
     <InstagramPreview {items} />
   </section>
